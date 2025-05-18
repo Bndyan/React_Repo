0ffb12648b97bed265d38a8c0ae60d7adf94afa6
Karwan Karasool Hasan Bndyan <k.bndyan@itbh.local>
2025-05-18 21:04:16 +0200

    The end of my project

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './contact.css';
 import Amazone from "../../assets/amazon.png";
 import Microsoft from "../../assets/microsoft.png";
 import Adobe from "../../assets/adobe.png";
 import Facebook from "../../assets/facebook_1.png";
-
 import facebook from "../../assets/facebook.png";
 import Instagram from "../../assets/instagram.png";
 import Twitter from "../../assets/twitter.png";
 import Youtub from "../../assets/youtube.png";
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+    // const form = useRef();
+    // const sendEmail = (e) => {
+    //     e.preventDefault();
+    
+    //     emailjs
+    //       .sendForm('service_7fph1ao', 'template_8lu5g3n', form.current, {
+    //         publicKey: 'LaX5OxbbFTWLBpfoHu9vK',
+    //       })
+    //       .then(
+    //         () => {
+    //           console.log('SUCCESS!');
+    //         },
+    //         (error) => {
+    //           console.log('FAILED...', error.text);
+    //         },
+    //       );
+    //   };
   return (
     <section id='contactPage'>
         <div id='clients'>
@@ -31,10 +48,14 @@ const Contact = () => {
             <h1 className='contactPageTile'>Contact Me</h1>
             <span className='contactDesc' >Please fill out the form below to discuss any work opportunies.</span>
 
-            <form className='contactForm'>
-                <input type='name' className='name' placeholder='Your Name' />
-                <input type='email' className='email' placeholder='Your Email' />
-                <textarea className='msg' type='message' rows="5" placeholder='Your Message' ></textarea>
+            <form className='contactForm'   
+                action="https://formspree.io/f/mvgaledn"
+                method="POST">
+
+
+                <input type='text' className='name' placeholder='Your Name' name='name'/>
+                <input type='email' className='email' placeholder='Your Email' name='email'/>
+                <textarea className='msg' name='message' rows="5" placeholder='Your Message' ></textarea>
 
                 <button type='submit' value="Send" className='submitBtn'>Submit</button>
 
