767278b6be63ad02e7ed9e344ba1b5a3af1fa3d5
Karwan Karasool Hasan Bndyan <k.bndyan@itbh.local>
2025-05-18 14:10:31 +0200

    add

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import logo from '../../assets/logo.png';
-import contactImg from '../../assets/message_chat.png'
+import contactImg from '../../assets/message_chat.png';
 import {Link} from 'react-scroll';
+import menu from '../../assets/menu1.png';
 
 
 const Navbar = () => {
+  const[showMenu ,setShowMenu] = useState(false);
   return (
     <nav className='navbar'>
       <img src={logo} alt="Logo" className='logo'/>
@@ -18,7 +20,16 @@ const Navbar = () => {
       <button className='desktopMenuBtn' onClick={()=>{
         document.getElementById('contact').scrollIntoView({behavior: 'smooth'});
       }}>
-        <img src={contactImg} alt='' className='desktopMenuImg' />Contact Me</button>
+        <img src={contactImg} alt='Menu' className='desktopMenuImg' />Contact Me</button>
+
+      <img src={menu} alt="Menu" className='mobMenu' onClick={()=>setShowMenu(!showMenu)}/>
+      <div className='navMenu' style={{display: showMenu? 'flex': 'none'}}>
+          <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-50} duration={500} className='listItem' onClick={()=>setShowMenu(false)}>Home</Link>
+          <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-50} duration={500} className='listItem' onClick={()=>setShowMenu(false)}>Portfolio</Link>
+          <Link activeClass='active' to='works' spy={true} smooth={true} offset={-50} duration={500} className='listItem' onClick={()=>setShowMenu(false)}>About</Link>
+          <Link activeClass='active' to='clients' spy={true} smooth={true} offset={-50} duration={500} className='listItem' onClick={()=>setShowMenu(false)}>Clients</Link>
+          <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-50} duration={500} className='listItem' onClick={()=>setShowMenu(false)}>Contact</Link>
+      </div>
     </nav>
   )
 }
